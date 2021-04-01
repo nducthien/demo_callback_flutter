e1f799b9e25bd7217d3bb3e9911b170526c2500b
nducthien <thien.chotrax23@gmail.com>
2021-04-01 18:26:40 +0700

    call callback in flutter

---
+++ android/app/src/main/kotlin/bb/team/demo_call_back/MainActivity.kt
@@ -0,0 +1,6 @@
+package bb.team.demo_call_back
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/src/screens/home.dart
@@ -0,0 +1,11 @@
+import 'package:demo_call_back/src/widgets/my_board.dart';
+import 'package:flutter/material.dart';
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: MyBoard(),
+    );
+  }
+}
